d6a298b47ee91c1c3936dd1815f38ad68593c3f6
Matt <botzmatt@yahoo.com>
2016-06-11 12:54:48 -0700

    adds two more destinations and initial variables defined in scripts.js. Also adds hide styling for destination results

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $("form").submit(function() {
+    event.preventDefault();
+    var answer1 = $("#quest2").val();
+    var answer2 = $("#quest3").val();
+    var answer3 = $("#quest4").val();
+    var answer4 = $("#quest5").val();
+    var result;
+  });
+});
